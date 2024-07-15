ebe5485cf36f249f51a2d357913152ddca4c4c1c
hazeco <thunderwolf2209@gmail.com>
2024-07-15 14:03:34 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('FROM KHET').split('')
+    const titles = ('White flower for you').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
