415aee50328d7cfe5611536b0b1916d0bab13277
hazeco <thunderwolf2209@gmail.com>
2024-08-13 21:58:50 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,15 +1,22 @@
-const title = document.querySelector('.title')
-const text = 'I Have Something for you'.split('')
-for (let index = 0; index < text.length; index++) {
-  if (text[index] !== ' ') {
-    title.innerHTML += `<span>${text[index]}<span/>`
-  } else {
-    title.innerHTML += `<span style='margin-right: 20px;'><span/>`
-  }
-}
 
-const textElements = document.querySelectorAll('.title span');
-textElements.forEach((element) => {
-  const randomDelay = Math.random() * 3; // Menghasilkan delay acak antara 0 hingga 3 detik
-  element.style.animationDelay = `${randomDelay}s`;
-});
+onload = () => {
+  const c = setTimeout(() => {
+    document.body.classList.remove("not-loaded");
+
+    const titles = ('To Fayya').split('')
+    const titleElement = document.getElementById('title');
+    let index = 0;
+
+    function appendTitle() {
+      if (index < titles.length) {
+        titleElement.innerHTML += titles[index];
+        index++;
+        setTimeout(appendTitle, 300); // 1000ms delay
+      }
+    }
+
+    appendTitle();
+
+    clearTimeout(c);
+  }, 1000);
+};
